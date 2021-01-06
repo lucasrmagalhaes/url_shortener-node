3c0fcd59cf2ca5435526aa54c897866676e0b026
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-06 18:19:43 -0300

    mongo conexao

---
+++ src/database/MongoConnection.ts
@@ -0,0 +1,14 @@
+import { config } from '../config/Constants'
+import mongoose from 'mongoose'
+
+export class MongoConnection {
+    public async connect(): Promise<void> {
+        try {
+            await mongoose.connect(config.MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true})
+            console.log('Database connected')
+        } catch(err) {
+            console.error(err.message)
+            process.exit(1)
+        }
+    }
+}
